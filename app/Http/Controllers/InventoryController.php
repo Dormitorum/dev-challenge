a883d25d17c40c180a78075a1a287d3f3cea5ecb
crislemen <crislemen@gmail.com>
2025-09-14 14:18:27 +0100

    Calculado stock agregado por producto/almacen , vista en las tablas tanto de stock como movimientos y filtro para los movimientos por producto y almacen

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Warehouse;
+use App\Models\InventoryMovement;
 use App\Services\InventoryService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class InventoryController extends Controller
 {
@@ -17,13 +19,40 @@ class InventoryController extends Controller
         $this->inventoryService = $inventoryService;
     }
 
-    public function index()
-    {
-        $products = Product::all();
-        $warehouses = Warehouse::all();
+public function index(Request $request)
+{
+    // Obtener los parámetros de filtrado
+    $productId = $request->input('product_id');
+    $warehouseId = $request->input('warehouse_id');
 
-        return view('inventory', compact('products', 'warehouses'));
+    $query = InventoryMovement::with(['product', 'warehouse'])
+        ->orderBy('id', 'desc');
+
+    if ($productId) {
+        $query->where('product_id', $productId);
     }
+
+    if ($warehouseId) {
+        $query->where('warehouse_id', $warehouseId);
+    }
+    $movements = $query->paginate(5);
+    $movements->appends(request()->query());
+
+    $products = Product::all();
+    $warehouses = Warehouse::all();
+
+    //Query para calcular el stock
+    $stockProductAndWarehouse = InventoryMovement::selectRaw("
+        product_id,
+        warehouse_id,
+        SUM(CASE WHEN type ='IN' THEN quantity ELSE 0 END)-
+        SUM(CASE WHEN type ='OUT' THEN quantity ELSE 0 END) AS stock"
+    )
+    ->groupBy('product_id', 'warehouse_id')
+    ->with(['product', 'warehouse'])
+    ->get();
+    return view('inventory', compact('products', 'warehouses', 'movements', 'stockProductAndWarehouse'));
+}
 
     public function store(Request $request)
     {
